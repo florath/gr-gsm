2e1ca60c1a9269f2913c5dd597be85c98ef5e5ab
Andreas Florath <andreas@florath.net>
2015-05-01 21:05:29 +0200

    Burst output format

--- lib/misc_utils/bursts_file_sink_impl.cc
+++ lib/misc_utils/bursts_file_sink_impl.cc
@@ -24,13 +24,43 @@
 #endif
 
 #include <gnuradio/io_signature.h>
+#include <grgsm/gsmtap.h>
 #include "misc_utils/bursts_file_sink_impl.h"
+#include <sstream>
+#include <iomanip>
 
 namespace gr {
 namespace gsm {
 
 void bursts_file_sink_impl::bursts_process(pmt::pmt_t msg) {
-  std::cerr << "Bursts_print" << std::endl;
+  pmt::pmt_t const header_plus_burst = pmt::cdr(msg);
+
+  gsmtap_hdr const *const header =
+      (gsmtap_hdr *)pmt::blob_data(header_plus_burst);
+  int8_t const *const burst =
+      (int8_t *)(pmt::blob_data(header_plus_burst)) + sizeof(gsmtap_hdr);
+  size_t const burst_len =
+      pmt::blob_length(header_plus_burst) - sizeof(gsmtap_hdr);
+
+  std::stringstream out;
+  out << std::setw(3) << (uint16_t)header->version << "," << std::setw(3)
+      << (uint16_t)header->hdr_len << "," << std::setw(3)
+      << (uint16_t)header->type << "," << std::setw(1)
+      << (uint16_t)header->timeslot << "," << std::setw(5)
+      << be16toh(header->arfcn) << "," << std::setw(4)
+      << (int16_t)header->signal_dbm << "," << std::setw(4)
+      << (int16_t)header->snr_db << "," << std::setw(10)
+      << be32toh(header->frame_number) << "," << std::setw(3)
+      << (uint16_t)header->sub_type << "," << std::setw(3)
+      << (uint16_t)header->antenna_nr << "," << std::setw(3)
+      << (uint16_t)header->sub_slot << "," << std::setw(3)
+      << (uint16_t)header->res << ",";
+  for (int i(0); i < burst_len; ++i) {
+     out << std::setw(1) << static_cast<int>(burst[i]);
+  }
+  out << std::endl;
+
+  std::cout << out.str();
 }
 
 bursts_file_sink::sptr bursts_file_sink::make(std::string const &filename) {
